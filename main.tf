b656f4d23d870dea9834057c92af9b848d0c7fa3
Searge <ashikcygan@gmail.com>
2022-03-04 23:08:00 +0200

    TF validated

--- main.tf
+++ main.tf
@@ -16,55 +16,62 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name                    = "ddosGroup"
-  location                = "eastus"
+  name     = "ddosGroup"
+  location = "eastus"
 }
 
 # Create a virtual network
 resource "azurerm_virtual_network" "vnetA" {
-    name                  = "revenNet1"
-    address_space         = ["10.0.0.0/16"]
-    location              = azurerm_resource_group.rg.location
-    resource_group_name   = azurerm_resource_group.rg.name
+  name                = "ravenNet1"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
-resource "azurerm_virtual_network" "vnetB" {
-    name                  = "revenNet2"
-    address_space         = ["10.1.0.0/16"]
-    location              = azurerm_resource_group.rg.location
-    resource_group_name   = azurerm_resource_group.rg.name
+
+resource "azurerm_subnet" "vsubA" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnetA.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
-resource "azurerm_virtual_network" "vnetC" {
-    name                  = "revenNet3"
-    address_space         = ["10.2.0.0/16"]
-    location              = azurerm_resource_group.rg.location
-    resource_group_name   = azurerm_resource_group.rg.name
+
+resource "azurerm_network_interface" "ngwA" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vsubA.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 # Create a virtual machines
 resource "azurerm_linux_virtual_machine" "vmA" {
-  name                    = "reven-vm1"
-  resource_group_name     = azurerm_resource_group.rg.name
-  location                = azurerm_resource_group.rg.location
-  size                    = "Standard_F2"
-  admin_username          = "adminuser"
-  network_interface_ids   = [
-    azurerm_virtual_network.vnetA,
+  name                = "raven-vm1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.ngwA.id,
   ]
 
   admin_ssh_key {
-    username              = "adminuser"
-    public_key            = file("~/.ssh/id_rsa.pub")
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
   }
 
   os_disk {
-    caching               = "ReadWrite"
-    storage_account_type  = "Standard_LRS"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
-    publisher             = "OpenLogic"
-    offer                 = "CentOS"
-    sku                   = "7.4"
-    version               = "latest"
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "7.4"
+    version   = "latest"
   }
 }
