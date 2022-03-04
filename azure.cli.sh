110a1b75c5273470118b5d035a4fe07cd1d6784d
Searge <ashikcygan@gmail.com>
2022-03-04 21:22:58 +0200

    >>inventory/hosts.all

--- azure.cli.sh
+++ azure.cli.sh
@@ -3,7 +3,6 @@
 resourceGroup=ddosgroup
 instlocation=eastus
 
-
 az group create --name $resourceGroup --location eastus
 
 az network vnet create \
@@ -25,37 +24,37 @@ az network vnet create \
   --subnet-name revensubnet2 \
   --subnet-prefixes 10.1.0.0/24
 az vm create \
-   --resource-group $resourceGroup \
-   --name revenvm1 \
-   --image OpenLogic:CentOS:7.4:latest \
-   --vnet-name revennet1 \
-   --subnet revensubnet1 \
-   --admin-username alex \
-   --admin-password my-super-puper-password \
-   --output json \
-   --verbose
+  --resource-group $resourceGroup \
+  --name revenvm1 \
+  --image OpenLogic:CentOS:7.4:latest \
+  --vnet-name revennet1 \
+  --subnet revensubnet1 \
+  --admin-username alex \
+  --admin-password my-super-puper-password \
+  --output json \
+  --verbose
 az vm create \
-   --resource-group $resourceGroup \
-   --name revenvm2 \
-   --image OpenLogic:CentOS:7.4:latest \
-   --vnet-name revennet2 \
-   --subnet revensubnet2 \
-   --admin-username alex \
-   --admin-password my-super-puper-password \
-   --output json \
-   --verbose
+  --resource-group $resourceGroup \
+  --name revenvm2 \
+  --image OpenLogic:CentOS:7.4:latest \
+  --vnet-name revennet2 \
+  --subnet revensubnet2 \
+  --admin-username alex \
+  --admin-password my-super-puper-password \
+  --output json \
+  --verbose
 az vm create \
-   --resource-group $resourceGroup \
-   --name revenvm3 \
-   --image OpenLogic:CentOS:7.4:latest \
-   --vnet-name revennet3 \
-   --subnet revensubnet3 \
-   --admin-username alex \
-   --admin-password my-super-puper-password \
-   --output json \
-   --verbose
+  --resource-group $resourceGroup \
+  --name revenvm3 \
+  --image OpenLogic:CentOS:7.4:latest \
+  --vnet-name revennet3 \
+  --subnet revensubnet3 \
+  --admin-username alex \
+  --admin-password my-super-puper-password \
+  --output json \
+  --verbose
 
-echo "[docker]" > host.txt
-az network public-ip list | grep ipAd | grep -v null | awk -F ":" '{print $2}' | cut -c 3- | rev | cut -c 3- | rev >> host.txt
+echo "[docker]" >inventory/hosts.all
+az network public-ip list | grep ipAd | grep -v null | awk -F ":" '{print $2}' | cut -c 3- | rev | cut -c 3- | rev >>inventory/hosts.all
 sleep 3800
 az group delete -g ddosgroup
