698e2b786832a1b54813c8cdfb056b3ee2e44416
Searge <ashikcygan@gmail.com>
2022-03-05 20:37:59 +0200

    terraform variables

--- variables.tf
+++ variables.tf
@@ -10,12 +10,12 @@ variable "script_path" {
   default = "./scripts/initialize_setup.sh"
 }
 
-variable "public_key" {
-  description = "The path to the private key used to connect to the instance"
+variable "username" {
+  description = "The name of the user that will be used to remote exec the script"
   default     = "toor"
 }
 
-variable "username" {
-  description = "The name of the user that will be used to remote exec the script"
+variable "public_key" {
+  description = "The path to the private key used to connect to the instance"
   default     = "~/.ssh/id_rsa.pub"
 }
